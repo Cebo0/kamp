d16b08318a136f8e9efd8758e9618a9ca4655213
Servet Ceberut <servet.ceberut@gmail.com>
2021-12-02 09:58:25 +0300

    1

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,20 +1,56 @@
-export default class UserService{
-    constructor(loggerService){
-        this.users = []
-        this.loggerService =loggerService
-    }
+import { users } from "../data/users.js";
+import DataError from "../models/dataError.js";
 
-    add(user){
-        this.users.push(user)
-        this.loggerService.log(user)
+export default class UserService {
+  constructor(loggerService) {
+    this.employees = [];
+    this.customers = [];
+    this.errors = [];
+    this.loggerService = loggerService;
+  }
+  load() {
+    for (const user of users) {
+      switch (user.type) {
+        case "customer":
+          if (!this.checkUserValidtyForErrors(user)) {
+                this.customers.push(user)
+          }
+          break;
+        case "employee":
+            if (!this.checkUserValidtyForErrors(user)) {
+                this.employees.push(user)
+          }
+          break;
+        default:
+          this.errors.push(new DataError("wrong user type", user));
+          break;
+      }
     }
-
-    list(){
-        return this.users
+  }
+  checkUserValidtyForErrors(user) {
+    let requiredFields = "id firstName lastName age city".split(" ");
+    let hasErrors = false;
+    
+    for (const field of requiredFields) {
+      if (!user[field]) {
+        hasErrors = true;
+        this.errors.push(
+          new DataError(`Validation porblem. ${field} is required`, user)
+        );
+      }
     }
+    return hasErrors;
+  }
+  add(user) {
+    // this.users.push(user)
+    this.loggerService.log(user);
+  }
 
-    getById(id){
-        return this.users.find(u=>u.id ===id)
-    }
+  list() {
+    // return this.users
+  }
 
+  getById(id) {
+    // return this.users.find(u=>u.id ===id)
+  }
 }
